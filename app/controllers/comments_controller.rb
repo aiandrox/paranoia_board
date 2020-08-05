bedaca82a116d4d5d78703a9cba51746f96febf8
Aiandrox <aiandrox7@gmail.com>
2020-08-05 21:38:16 +0900

    [Add] cotoha gemを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,21 @@
 class CommentsController < ApplicationController
   def create
-  end
-
-  def update
-    
+    if current_user.comments.create(comment_params)
+      redirect_to 'static_pages#top'
+    else
+      render 'static_pages/top'
+    end
   end
 
   def destroy
+    comment = current_user.comments.find(params[:id])
+    comment.destroy!
+    redirect_to 'static_pages#top'
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,24 @@
 class Comment < ApplicationRecord
+  before_validation :filter_body
   belongs_to :user
-
   validates :body, presence: true
+
+  private
+
+  def filter_body
+    response
+    debugger
+  end
+
+  def response
+    cotoha_client.sentiment(sentence: body)
+  end
+
+  def cotoha_client
+    client_id = Rails.application.credentials.cotoha[:client_id]
+    client_secret = Rails.application.credentials.cotoha[:client_secret]
+    client = Cotoha::Client.new(client_id: client_id, client_secret: client_secret)
+    client.create_access_token
+    client
+  end
 end
