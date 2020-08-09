e91852f76c6e5d65b1e4b56d16f68223961886eb
Aiandrox <aiandrox7@gmail.com>
2020-08-09 21:32:18 +0900

    [Change] 名前の変更を別アクションにする

---
+++ app/controllers/mypage/first_names_controller.rb
@@ -0,0 +1,17 @@
+class Mypage::FirstNamesController < ApplicationController
+  def update
+    @comments = current_user.comments
+    if current_user.update(user_params)
+      redirect_to mypage_path, success: '成功'
+    else
+      flash.now[:error] = '失敗'
+      render :show
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   resources :comments, only: %i[create update destroy]
   resource :mypage, only: %i[show update]
+  namespace :mypage do
+    resource :first_name, only: %i[update]
+  end
 end
